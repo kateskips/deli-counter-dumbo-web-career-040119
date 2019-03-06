c592733d685ffe70c0d9dff99dee7aacdc69a29d
Kate F <follett.katie@gmail.com>
2019-03-06 20:52:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,41 @@
 require 'pry'
 
 katz_line = []
-other_deli = ["Logan", "Avi", "Spencer"]
-another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+#other_deli = ["Logan", "Avi", "Spencer"]
+#another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
-def line(array)
-  counter = 1
-  counter +=1
-  if array.length == 0
-    puts "The line is currently empty."
-    elsif array.length > 0
-    puts "The line is currently:" + "#{counter}" + "#{other_deli}" 
-  end
+#def line(array)
+#  counter = 1
+ # counter +=1
+ # if array.length == 0
+   # puts "The line is currently empty."
+   # elsif array.length > 0
+   # puts "The line is currently:" + "#{counter}" + "#{other_deli}" 
+  #end
  
+#end
+
+def take_a_number(katz_line,name)
+  katz_line.push(name)
+  puts "Welcome, #{name}. You are number #{katz_line.length} in line."
+end
+
+
+def now_serving(katz_line)
+ if katz_line.length == 0
+   puts "There is nobody waiting to be served!"
+   elsif katz_line.length > 0 
+   puts "Currently serving #{katz_line.shift}."
+ end
+end
+
+
+def line(katz_line)
+  number = 0
+  number +=1
+  if katz_line.length == 0
+    puts "The line is currently empty."
+  elsif katz_line.length > 0
+  puts "The line is currently: #{number}.#{katz_line}"
+ end
 end
