f9941b30976c2f63fbc1d5ece2c2b6d8156e0e46
Kate F <follett.katie@gmail.com>
2019-03-06 21:06:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -34,8 +34,8 @@ def line(katz_line)
   if katz_line.length == 0
     puts "The line is currently empty."
   elsif katz_line.length > 0
-  katz_line.each do |name|
- binding.pry
+  katz_line.each_with_index do |name, index|
+
     end
   end
 end
