6fe87556dcf85c6697072f5e015a71828729b813
Kate F <follett.katie@gmail.com>
2019-03-06 21:01:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -34,8 +34,7 @@ def line(katz_line)
   if katz_line.length == 0
     puts "The line is currently empty."
   elsif katz_line.length > 0
-  katz_line.each_with_index do |name, number|
-    number += 1
+  katz_line.each do |key, value|
   puts "The line is currently: #{value}.#{katz_line}"
     end
   end
